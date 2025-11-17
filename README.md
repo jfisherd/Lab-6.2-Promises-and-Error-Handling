88966677ce7963d5af4ff289f4a3a11c2c102c56
Josh Fisher <jfisherd@gmail.com>
2025-11-17 15:44:18 -0500

    initial commit + Promise's added, configuring

---
+++ README.md
@@ -0,0 +1,28 @@
+
+
+# Lab 6.2, Promises and Error Handling
+
+## Description
+In this lab [ ]
+
+
+
+## Running the program
+To view use this lab [ ]
+<br>
+
+## Reflection Questions
+> Why is it important to handle errors for each individual API call rather than just at the end of the promise chain?
+
+
+<br>
+
+> How does using custom error classes improve debugging and error identification?
+
+
+<br>
+
+> When might a retry mechanism be more effective than an immediate failure response?
+
+
+<br>
---
+++ apiSimulator.ts
@@ -0,0 +1,44 @@
+export const fetchProductCatalog = (): Promise<{ id: number; name: string; price: number }[]> => {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (Math.random() < 0.8) {
+        resolve([
+            { id: 1, name: "Laptop", price: 1200 },
+            { id: 2, name: "Headphones", price: 200 },
+        ]);
+        } else {
+        reject("Failed to fetch product catalog");
+        }
+    }, 1000);
+    });
+};
+
+const fetchProductReviews = (): Promise<{ id: number; name: string; price: number }[]> => {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (Math.random() < 0.8) {
+        resolve([
+            { id: 1, name: "Laptop", price: 1200 },
+            { id: 2, name: "Headphones", price: 200 },
+        ]);
+        } else {
+        reject("Failed to fetch reviews for product ID ${productId}");
+        }
+    }, 1500);
+    });
+};
+
+const fetchSalesReport = (): Promise<{ id: number; name: string; price: number }[]> => {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (Math.random() < 0.8) {
+        resolve([
+            { id: 1, name: "Laptop", price: 1200 },
+            { id: 2, name: "Headphones", price: 200 },
+        ]);
+        } else {
+        reject("Failed to fetch sales report");
+        }
+    }, 1000);
+    });
+};
